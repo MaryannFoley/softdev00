ca55fdd65989780e0e342b484b4cd2eeb0343968
Maryann Foley <mfoley@stuy.edu>
2018-11-13 17:08:17 -0500

    Finish website

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import urllib.request
+import json
 app = Flask(__name__)
 
 
@@ -7,9 +8,17 @@ app = Flask(__name__)
 def something():
     request=urllib.request.urlopen("https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=4Gt3nL0hLF4rNbj9aifudBMdz0alxDewjaOIN2Rx")
     raw=request.read()
-    json=raw.loads()
-    print(json)
-    return "<h3>Homework template</h3>"
+    print(raw)
+    jdict=json.loads(raw)
+    print(jdict)
+    url = jdict["url"]
+    print(url)
+    planet=jdict["resource"]["planet"].upper()
+    date=jdict["date"]
+    date=date.split("T")
+
+
+    return render_template("index.html",img=url, planet=planet,date=date)
     #render_template is also useful
 
 if __name__ == "__main__":
