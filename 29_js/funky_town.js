5b32a51091b5aecbcd42b26b526eacde23b8b21a
Maryann Foley <mfoley@stuy.edu>
2018-12-19 22:59:05 -0500

    Changes inner html

--- 29_js/funky_town.js
+++ 29_js/funky_town.js
@@ -1,43 +1,56 @@
-/*Team Law of C - Wei Wen Zhou, Maryann Foley
+/*Team MAD - Anton Danylenko, Maryann Foley
 SoftDev1 pd8
 K28 -- Sequential Progression
 2018-12-18
 */
-var fibonacci = function(n) {
-    if (n == 0) { 
-        return 0;
-    }
-    if (n < 3) { // return 1 for 1 and 2
-        return 1;
-    } else {
-        return fibonacci(n-1) + fibonacci(n-2);
-    }
+
+var fibonacci = (n) => {
+  if (n == 0)
+    return 0;
+  else if (n == 1)
+    return 1;
+  else
+    return fibonacci(n - 2) + fibonacci(n - 1);
+};
+
+var gcd = (a, b) => {
+  if (a > b) {
+    for (x = b; x > 0; x--)
+      if (((a % x) == 0) && ((b % x) == 0))
+        return x;
+  }
+  else {
+    for (x = a; x > 0; x--)
+      if (((a % x) == 0) && ((b % x) == 0))
+        return x;
+  }
 }
 
-var gcd = function(a, b) {
-    var x;
-    if (a < b) { //begin with the smaller of the 2 numbers
-        x = a;
-    } else {
-        x = b;
-    }
-    while (x > 0) { //starting from there, check if each number is a cd
-        if (a % x == 0 && b % x == 0) {
-            return x;
-        }
-        x--;
-    }
-}
+var students = ["bob", "sally", "john", "john2"];
 
-var randomStudent = function() {
-    var list = ['Derek', 'Britni', 'Joan', 'Vincent', 'Jared', 'Ivan', 'Thomas', 'Maggie', 'Damian', 'Tina', 'Fabiha', 'John', 'Susan ', 'Kaitlin', 'Michelle', 'Clara', 'Rachel', 'Amit', 'Jerry', 'Raymond', 'Zane', 'Soojin', 'Maryann', 'Adil', 'Josh', 'Imad' ]; //Retrived from a prev homework
-    return list[Math.floor(Math.random()*list.length)]; //Get a random element from the list
+var randomNumber = () => {
+  return Math.floor(Math.random() * 3)
 }
-
+var randomStudent = () => {
+  randIndex = randomNumber();
+  return students[randIndex];
+}
 
 var b0 = document.getElementById("b0");
 var b1 = document.getElementById("b1");
 var b2 = document.getElementById("b2");
-b0.addEventListener('click', fibonacci(5));
-b1.addEventListener('click', gcd(15, 255));
-b2.addEventListener('click', randomStudent);
+var p0 = document.getElementById("p0");
+var p1 = document.getElementById("p1");
+var p2 = document.getElementById("p2");
+b0.addEventListener('click', function(){
+  console.log(fibonacci(5));
+  p0.innerHTML=fibonacci(5);
+});
+b1.addEventListener('click', function(){
+  console.log(gcd(15, 255));
+  p1.innerHTML=gcd(15, 255);
+});
+b2.addEventListener('click', function(){
+  console.log(randomStudent());
+  p2.innerHTML=randomStudent();
+});
